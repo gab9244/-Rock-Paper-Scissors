28ec67adf1f5d4d4453e59ade4c061420ad608dd
gab9244 <gab9244@gmail.com>
2022-11-13 08:32:56 -0300

    Projeto quase acabado, a unica coisa que falta é fazer com que o jogo acaba em 5 partidas

--- JavaScript.js
+++ JavaScript.js
@@ -1,87 +1,58 @@
-const game = ["Rock","Paper","Scissors"]
+ 
+const Choices = ["rock","paper","scissors"]
 function getComputerChoice() {
 
- return game[Math.floor(Math.random()*game.length)]
+ return Choices[Math.floor(Math.random()*Choices  .length)]
 }
+let PlayerPoints_span = 0
+let ComputerPoints_span = 0
+const placar = document.getElementsByClassName('placar')
+const PlayerPoints = document.getElementById("PlayerPoints")
+const ComputerPoints = document.getElementById("ComputerPoints")
+const Rock_div = document.getElementById("Rock_div")
+const Paper_div = document.getElementById("Paper_div")
+const Scissors_div = document.getElementById("Scissors_div")
+const result = document.getElementById('result')
 
-
-// function playonce(playerSelection,computerSelection){
-
-    playerSelection=getComputerChoice()
-    computerSelection=getComputerChoice()
-
-    // if (computerSelection== game[0] && playerSelection == game[2]) {
-    //         return "You Lose!, Rock beats Scissors"
-            
-    // }
-    // if (computerSelection== game[1] && playerSelection == game[0]) {
-    //     return "You Lose!, Paper beats Rock "
-        
-    // }
-    // if (computerSelection== game[2] && playerSelection == game[1]) {
-    //     return "You Lose!, Scissors beats Paper"
-        
-    // }
-// if (playerSelection ==game[0] && computerSelection == game[2]){
-//     return "You Win!, Rock beats Scissors"
-// }
-// if (playerSelection ==game[1] && computerSelection == game[0]){
-//     return "You win!, Paper beats Rock "
-// }
-// if (playerSelection ==game[2] && computerSelection == game[1]){
-//     return "You win!, Scissors beats Paper"
-// }
-// }
-
-let you = document.getElementById("you")
-let youwin = document.getElementById("youwin")
-
-function yourchoice(){
-   
-    prompt(`Choose between: 
-    Rock(1)
-    Paper(2)
-    Scissors(3)`)
-    
-}
-
-
- function games(){
-        playerSelection = 1,2,3
-        computerSelection = getComputerChoice()
-        for (var i = 1; i<= 5; i++){
-            if (playerSelection ==1 && computerSelection == game[2]){
-                return "You Win!, Rock beats Scissors"
-            }
-            if (playerSelection ==2 && computerSelection == game[0]){
-                return "You win!, Paper beats Rock "
-            }
-            if (playerSelection ==3 && computerSelection == game[1]){
-                return "You win!, Scissors beats Paper"
-            }
-
-        }
-        for(var e = 1; e <5;e++) {
-        if (computerSelection== game[0] && playerSelection == 3) {
-                return "You Lose!, Rock beats Scissors"
-                
-        }
-        if (computerSelection== game[1] && playerSelection == 1) {
-            return "You Lose!, Paper beats Rock "
-            
-        }
-        if (computerSelection== game[2] && playerSelection == 2) {
-            return "You Lose!, Scissors beats Paper"
-            
-        }
-
-        }
-        if(i>e) {
-            youwin.innerHTML = `Player wins with ${i} victories`
-        }
-        if(e>i) {
-            youwin.innerHTML= `Computer wins with ${e} victories`
-        }
+function games(PlayerChoice) {
+ const computerChoice = getComputerChoice()
+ 
+ if(PlayerChoice=='rock'&& computerChoice =='scissors'||
+    PlayerChoice == 'paper'&& computerChoice == 'rock' ||
+    PlayerChoice == 'scissors' && computerChoice == 'paper'
+ ){
+  PlayerPoints.innerHTML=  PlayerPoints_span +=1
  }
+ else if(computerChoice =='rock' && PlayerChoice == 'scissors' ||
+        computerChoice == 'paper' && PlayerChoice =='rock' ||
+        computerChoice == 'scissors' && PlayerChoice =='paper'
+ ){
+  ComputerPoints.innerHTML = ComputerPoints_span +=1
+ }
+ else {
+  console.log('Tie')
+ }
+}
+//Função que quarda os 3 botões
+function main (){
+  // Usei o addEventListener no botão para que algo aconteça quando ele for clicado
+Rock_div.addEventListener('click', function()  {
+  //Os valores de cada botão substituirão o parãmetro da função games pelos seus valores quando clicados
+  games('rock');
+    });
+    
+Paper_div.addEventListener('click', function()  {
+  games('paper');
+});
+
+Scissors_div.addEventListener('click', function()  {
+  games('scissors');
+    })}
+    
+//Funçõa em escopo global
+main();
+
+
+
 
 
