9f2410528539627a07a7b11eebe594442b45b555
gab9244 <gab9244@gmail.com>
2022-11-25 19:42:12 -0300

    Agora tem um

--- JavaScript.js
+++ JavaScript.js
@@ -1,57 +1,60 @@
- 
+const PlayerPoints = document.querySelector("#PlayerPoints")
+const ComputerPoints = document.querySelector("#ComputerPoints")
+const Rock_div =document.getElementById("Rock_div")
+const Paper_div =document.getElementById("Paper_div")
+const Scissors_div =document.getElementById("Scissors_div")
+const result = document.querySelector('#result')
+
 const Choices = ["rock","paper","scissors"]
 function getComputerChoice() {
 
  return Choices[Math.floor(Math.random()*Choices  .length)]
 }
-let PlayerPoints_span = 0
-let ComputerPoints_span = 0
-const placar = document.getElementsByClassName('placar')
-const PlayerPoints = document.getElementById("PlayerPoints")
-const ComputerPoints = document.getElementById("ComputerPoints")
-const Rock_div = document.getElementById("Rock_div")
-const Paper_div = document.getElementById("Paper_div")
-const Scissors_div = document.getElementById("Scissors_div")
-const result = document.getElementById('result')
+let playerp = 0
+let computerp = 0
+function Round(PlayerSelection,ComputerSelection) {
+  
+  ComputerSelection = getComputerChoice()
+   if(
+    PlayerSelection == 'rock' && ComputerSelection =='scissors'|| 
+    PlayerSelection == 'paper' && ComputerSelection =='rock'||
+    PlayerSelection == 'scissors' && ComputerSelection =='paper') {
+    PlayerPoints.textContent = playerp +=1
+   }
+    else if( 
+      ComputerSelection == 'rock' && PlayerSelection == 'scissors'||
+      ComputerSelection == 'paper' && PlayerSelection == 'rock'||
+      ComputerSelection == 'scissors' && PlayerSelection == 'paper'
+    ){
+      ComputerPoints.textContent = computerp +=1
+    }
+    if (playerp ==5) {
+      result.textContent = 'O jogador venceu'
+    }
+    else if( computerp == 5) {
+         result.textContent = 'O computador venceu'
+    }
 
-function games(PlayerChoice) {
- const computerChoice = getComputerChoice()
- 
- if(PlayerChoice=='rock'&& computerChoice =='scissors'||
-    PlayerChoice == 'paper'&& computerChoice == 'rock' ||
-    PlayerChoice == 'scissors' && computerChoice == 'paper'
- ){
-  PlayerPoints.innerHTML=  PlayerPoints_span +=1
- }
- else if(computerChoice =='rock' && PlayerChoice == 'scissors' ||
-        computerChoice == 'paper' && PlayerChoice =='rock' ||
-        computerChoice == 'scissors' && PlayerChoice =='paper'
- ){
-  ComputerPoints.innerHTML = ComputerPoints_span +=1
- }
- else {
-  console.log('Tie')
- }
 }
-//Função que quarda os 3 botões
-function main (){
+
+
+
+
   // Usei o addEventListener no botão para que algo aconteça quando ele for clicado
 Rock_div.addEventListener('click', function()  {
   //Os valores de cada botão substituirão o parãmetro da função games pelos seus valores quando clicados
-  games('rock');
+  Round('rock');
     });
     
 Paper_div.addEventListener('click', function()  {
-  games('paper');
+  Round('paper');
 });
 
 Scissors_div.addEventListener('click', function()  {
-  games('scissors');
-    })}
+  Round('scissors');
+    });
     
 //Funçõa em escopo global
-main();
-
 
 
 
